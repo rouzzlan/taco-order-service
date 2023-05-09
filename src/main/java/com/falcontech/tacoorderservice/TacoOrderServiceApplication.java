f69b65900f002c759f03a9708e0157068e1dad02
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-05-09 20:36:31 +0200

    initial commit

---
+++ src/main/java/com/falcontech/tacoorderservice/TacoOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.falcontech.tacoorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/falcontech/tacoorderservice/TacoOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcontech.tacoorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
