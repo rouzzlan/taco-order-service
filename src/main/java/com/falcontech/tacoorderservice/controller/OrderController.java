bf27ba2611f45574148afb1218458ae843b87621
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2023-06-01 12:31:44 +0200

    cors support added

--- src/main/java/com/falcontech/tacoorderservice/controller/OrderController.java
+++ src/main/java/com/falcontech/tacoorderservice/controller/OrderController.java
@@ -3,6 +3,10 @@ package com.falcontech.tacoorderservice.controller;
 import com.falcontech.tacoorderservice.controller.handler.OrderHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -20,5 +24,20 @@ public class OrderController {
             path(ENDPOINT_PATH),
             builder -> builder.GET("/{id}", handler::getOrder).GET("", handler::getOrders))
         .build();
+  }
+  @Bean
+  CorsWebFilter corsWebFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    //    config.applyPermitDefaultValues()
+    //    config.setAllowCredentials(true);
+    config.addAllowedOrigin("http://localhost:4200");
+    config.addAllowedOrigin("http://taco-order.service:*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod(HttpMethod.GET);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsWebFilter(source);
   }
 }
